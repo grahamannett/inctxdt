c4c4cb30fa99fc828f74d1985de0fedbcf1e496c
graham <graham.annett@gmail.com>
2023-09-12 22:58:04 -0600

    use discretization for actions

--- inctxdt/experiment_types.py
+++ inctxdt/experiment_types.py
@@ -5,8 +5,6 @@ from inctxdt.models.model import DecisionTransformer
 from inctxdt.trainer import train
 
 
-
-
 def dataloader_from_dataset(dataset, dataloader=None, config=None, accelerator=None):
     if dataloader:
         return dataloader
@@ -62,6 +60,22 @@ def run_autoregressive(config, dataset=None, dataloader=None, accelerator=None, 
         env_spec=env_spec,
         EmbedClass=config.embed_class,
     )
+
+    batch = next(
+        iter(
+            DataLoader(
+                dataset,
+                batch_size=config.batch_size * 4,
+                shuffle=config.shuffle,
+                num_workers=config.num_workers,
+                collate_fn=Collate(
+                    batch_first=True,
+                    device=None if accelerator else config.device,
+                    return_fn=dataset.collate_fn(batch_first=True),
+                ),
+            )
+        )
+    )
+
+    model.create_discretizer("actions", batch.actions)
     train(model, dataloader=dataloader, config=config, accelerator=accelerator, env_spec=env_spec, env=env, venv=venv)
-
-
