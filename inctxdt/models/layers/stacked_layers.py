2c56c5fa7c673a85ec3327ded30e2f26ef8935d1
graham <graham.annett@gmail.com>
2023-09-26 19:39:15 -0600

    not sure if need init on each submodule

--- inctxdt/models/layers/stacked_layers.py
+++ inctxdt/models/layers/stacked_layers.py
@@ -9,10 +9,6 @@ from inctxdt.models.layers.base_layers import BaseInputOutput, OriginalActionHea
 from inctxdt.models.layers.conv_embed_layers import ConvActionDimHead
 
 
-def _return_embeds(locs, to_return, embeds, padding_mask):
-    return embeds, padding_mask, {k: locs[k] for k in to_return}
-
-
 class BaseModalEmbedding(nn.Module):
     def _add_time_emb(self, time_emb: torch.Tensor) -> torch.Tensor:
         return time_emb
@@ -25,8 +21,20 @@ class BaseActionEmbedding(BaseModalEmbedding):
         self.embedding_dim = embedding_dim
         self._action_head = nn.Sequential(nn.Linear(self.embedding_dim, self.action_dim), nn.Tanh())
 
-    def action_head(self, hs: torch.Tensor, *args, **kwargs) -> torch.Tensor:
-        return self._action_head(hs[:, :, 1, :])
+        self.apply(self._init_weights)
+
+    @staticmethod
+    def _init_weights(module: nn.Module):
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if isinstance(module, nn.Linear) and module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.LayerNorm):
+            torch.nn.init.zeros_(module.bias)
+            torch.nn.init.ones_(module.weight)
+
+    def action_head(self, x: torch.Tensor, *args, **kwargs) -> torch.Tensor:
+        return self._action_head(x[:, :, 1, :])
 
 
 class ActionEmbedding(BaseActionEmbedding):
@@ -141,21 +149,28 @@ class SequentialAction(BaseInputOutput):
         self.state_dim = state_dim
         self.action_dim = action_dim
 
-        self.out_norm = nn.LayerNorm(self.embedding_dim)
         # self.conv = nn.Conv2d(embedding_dim, action_dim, kernel_size=kernel_size)
 
         # self.timestep_emb = nn.Embedding(episode_len + seq_len, embedding_dim)
         self.timestep_emb = nn.Embedding(episode_len * 2, self.embedding_dim)
         self.state_emb = nn.Linear(state_dim, self.embedding_dim)
-
         self.return_emb = nn.Linear(1, self.embedding_dim)
 
-        ActEmb = ModalEmbCls[self.modal_embed_config.action_embed_class]
+        self.action_emb = ModalEmbCls[self.modal_embed_config.action_embed_class](
+            action_dim=action_dim, embedding_dim=self.embedding_dim, **modal_embed_config.__dict__
+        )
 
-        self.action_emb = ActEmb(action_dim=action_dim, embedding_dim=self.embedding_dim, **modal_embed_config.__dict__)
+        self.apply(self._init_weights)
 
-        # output head
-        self.observation_head = nn.Sequential(nn.Linear(self.embedding_dim, state_dim), nn.Tanh())
+    @staticmethod
+    def _init_weights(module: nn.Module):
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if isinstance(module, nn.Linear) and module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.LayerNorm):
+            torch.nn.init.zeros_(module.bias)
+            torch.nn.init.ones_(module.weight)
 
     def forward_embed(
         self,
@@ -163,7 +178,6 @@ class SequentialAction(BaseInputOutput):
         actions: torch.Tensor,
         returns_to_go: torch.Tensor,
         timesteps: torch.Tensor,
-        padding_mask: torch.Tensor,
         to_return: list[str] = None,
         **kwargs,
     ):
@@ -174,39 +188,31 @@ class SequentialAction(BaseInputOutput):
 
         ret_emb = self.return_emb(returns_to_go.unsqueeze(-1))
         state_emb = self.state_emb(states)
+        act_emb = self.action_emb(actions)
 
         # since we are flattening the actions for some embeddings, we need to repeat the time embedding for each action in some cases
-        act_emb = self.action_emb(actions)
 
         # add time emb, since time emb might be need to be repeated, we need to repeat it for each action
         ret_emb += time_emb
         state_emb += time_emb
-
         act_emb += self.action_emb._add_time_emb(time_emb)
 
         # stack embeddings so that we have [bs, seq_len * spread_dim, embedding_dim]
         embeds = torch.cat([ret_emb, state_emb, act_emb], dim=1)
         self.spread_dim = embeds.shape[1] // seq_len
 
-        if padding_mask is not None:
-            # padding mask comes in as [bs, seq_len] -> want [bs, unpacked_sequence_len]
-            padding_mask = torch.stack([padding_mask for _ in range(self.spread_dim)], dim=-1).reshape(bs, -1)
-
-        return embeds, padding_mask
+        return embeds
 
     def forward_output(self, x: torch.Tensor, *args, **kwargs):
         """last layer of model, or what we learn to predict"""
-        bs = x.shape[0]
+        # bs = x.shape[0]
         # we want to reshape to reshape to [bs, seq_len, spread_dim, embedding_dim] because
         # the first value of each timestep corresponds to the state dim, regardless of how many actions
-        x = self.out_norm(x)
-        x = x.reshape(bs, self._batch_seq_len, -1, self.embedding_dim)
+        # x = x.reshape(bs, self._batch_seq_len, -1, self.embedding_dim)
 
         # 1 refers to state -> predict action
-        act_logits = self.action_emb.action_head(x)
-        obs_logits = self.observation_head(x[:, :, 2, :])
-
-        return act_logits, obs_logits
+        return self.action_emb.action_head(x)
+        # obs_logits = self.observation_head(x[:, :, 2, :])
 
 
 # KEEP THESE FOR POSTERITY UNTIL CERTAIN ITS WORKING
@@ -290,7 +296,6 @@ class AgnosticEmbed(BaseInputOutput):
         actions: torch.Tensor,
         returns_to_go: torch.Tensor,
         timesteps: torch.Tensor,
-        padding_mask: torch.Tensor,
         # **kwargs
     ):
         batch_size, seq_len = states.shape[0], states.shape[1]
@@ -316,12 +321,7 @@ class AgnosticEmbed(BaseInputOutput):
         embeds = torch.stack([ret_emb, state_emb, act_emb], dim=2)
         embeds = embeds.reshape(batch_size, -1, self.embedding_dim)
 
-        if padding_mask is not None:
-            # padding mask comes in as [batch_size, seq_len] -> want [batch_size, unpacked_sequence_len]
-            padding_mask = torch.stack([padding_mask for _ in range(3)], dim=-1)
-            padding_mask = padding_mask.reshape(batch_size, 3 * seq_len)
-
-        return embeds, padding_mask
+        return embeds
 
     def forward_output(self, x: torch.Tensor, *args, **kwargs):
         return self.action_head(x[:, 1::3])
