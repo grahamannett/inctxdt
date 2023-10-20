1369c28b979e413afb4295ec22e62ece42a0d3da
graham <graham.annett@gmail.com>
2023-10-20 12:19:00 -0600

    commit

--- sfns.sh
+++ sfns.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+DEVICE="${DEVICE:-0}"
+SEED="${SEED:-10}"
+
+GROUP="${GROUP:-DOWNSTREAM/AntmazeUmaze-TrainedHalfCheetahMediumExpert}"
+# CONFIG PATH is PRETRAIN
+CONFIG_PATH="${CONFIG_PATH:-conf/corl/dt/halfcheetah/medium_expert_v2.yaml}"
+DOWNSTREAM_CONFIG_PATH="${DOWNSTREAM_CONFIG_PATH:-conf/corl/dt/antmaze/umaze_v2.yaml}"
+UPDATE_STEPS="${UPDATE_STEPS:-50000}"
+
+NUM_LAYERS="${NUM_LAYERS:-3}"
+NUM_HEADS="${NUM_HEADS:-1}"
+
 
 run_docker() {
     echo running docker
